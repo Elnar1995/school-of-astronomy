2ed696e22d6808e207f84ccbefa0eb6e4a52fb16
Elnar1995 <51443597+Elnar1995@users.noreply.github.com>
2023-03-07 15:07:54 +0300

    Initial commit

---
+++ registration.php
@@ -0,0 +1,81 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Home - Brand</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i&amp;display=swap">
+    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.11.1/baguetteBox.min.css">
+    <link rel="stylesheet" href="assets/css/vanilla-zoom.min.css">
+    
+</head>
+<nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
+    <div class="container"><a class="navbar-brand logo" href="#">Онлайн школа астрономии</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navcol-1">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="index.html">Главная</a></li>
+                <li class="nav-item"><a class="nav-link" href="lessons.html">Уроки</a></li>
+                <li class="nav-item"><a class="nav-link" href="tests.php">Тесты</a></li>
+                <li class="nav-item"><a class="nav-link" href="news.php">Новости</a></li>
+                <li class="nav-item"><a class="nav-link" href="contact.html">Контакты</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+           
+<div class="content">
+<section class="vh-100 bg-image"
+  style="background-image: url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp');">
+  <div class="mask d-flex align-items-center h-100 gradient-custom-3">
+    <div class="container h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+          <div class="card" style="border-radius: 15px;">
+            <div class="card-body p-5">
+              <h2 class="text-uppercase text-center mb-5">Создать аккаунт</h2>
+
+              <form method="post" action="do_register.php">
+
+                <div class="form-outline mb-4">
+                  
+                  <label >Ваше имя</label>
+                  <input type="text" class="form-control" id="username" name="username" required>
+                </div>
+
+                
+
+                <div class="form-outline mb-4">
+                <input type="password" class="form-control" id="password" name="password" required>
+                  <label>Пароль</label>
+                </div>
+
+
+
+
+
+                <form method="post" action="do_register.php">
+                <div class="d-flex justify-content-center">
+                  <button type="button"
+                    class="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Регистрация</button>
+                </div>
+                </form>
+                <p class="text-center text-muted mt-5 mb-0">Уже есть аккаунт?<a href="login.html"  target="_blank"
+                    class="fw-bold text-body"><u>Войти здесь</u></a></p>
+
+              </form>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</div>
+
+</div>
+</body>
+
+</html>
---
+++ tests.php
@@ -0,0 +1,40 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Home - Brand</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i&amp;display=swap">
+    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.11.1/baguetteBox.min.css">
+    <link rel="stylesheet" href="assets/css/vanilla-zoom.min.css">
+</head>
+
+
+<nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
+        <div class="container"><a class="navbar-brand logo" href="#">Онлайн школа астрономии</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="index.html">Главная</a></li>
+                    <li class="nav-item"><a class="nav-link" href="lessons.html">Уроки</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="tests.php">Тесты</a></li>
+                    <li class="nav-item"><a class="nav-link" href="news.php">Новости</a></li>
+                    <li class="nav-item"><a class="nav-link" href="contact.html">Контакты</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+<html>
+    <body>
+        <div class='wrapper'>
+        <spacer width="500" height="100" type="block">
+            <p>wasdasdasdasd</p>
+<div class="content"></div>
+<div class='footer'>
+    <footer>
+        <p>© 2023</p>
+    </footer>
+</div>
+</div>
+</body>
+<link rel='stylesheet' href='assets/css/style.css' />
+</html>
